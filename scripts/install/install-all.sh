345e10d7a1b0ab77ff81abf0bfa8d961625cdb19
Trojanul <trojanul2008@gmail.com>
2025-08-02 18:59:32 +0300

    Rearange ci v2.

--- scripts/install/install-all.sh
+++ scripts/install/install-all.sh
@@ -2,18 +2,18 @@
 set -e
 
 echo "🔧 Installing Kubeconform..."
-scripts/install-kubeconform.sh
+scripts/install/install-kubeconform.sh
 
 echo "🛡️ Installing Kyverno..."
-scripts/install-kyverno.sh
+scripts/install/install-kyverno.sh
 
 echo "⛵ Installing Helm..."
-scripts/install-helm.sh
+scripts/install/install-helm.sh
 
 echo "📎 Installing YQ..."
-scripts/install-yq.sh
+scripts/install/install-yq.sh
 
 echo "🔍 Installing Trivy..."
-scripts/install-trivy.sh
+scripts/install/install-trivy.sh
 
 echo "✅ All tools installed successfully!"
