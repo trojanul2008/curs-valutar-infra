f2343e880264048d66aec677c5db108cfba349e0
Trojanul <trojanul2008@gmail.com>
2025-08-09 18:40:26 +0300

    fix: kyverno CLI download URL to match release naming

--- scripts/install/install-kyverno-cli.sh
+++ scripts/install/install-kyverno-cli.sh
@@ -2,9 +2,15 @@
 set -e
 source ./scripts/utils/load-version.sh
 
-VERSION=$(get_version kyvernoApp)   # e.g., 1.12.4
+VERSION=$(get_version kyvernoApp)
 OS="linux"
-ARCH="amd64"
+
+ARCH_RAW=$(uname -m)
+case "$ARCH_RAW" in
+  x86_64) ARCH="x86_64" ;;
+  aarch64|arm64) ARCH="arm64" ;;
+  *) echo "❌ Unsupported arch: $ARCH_RAW"; exit 1 ;;
+esac
 
 BINARY="kyverno-cli_v${VERSION}_${OS}_${ARCH}.tar.gz"
 URL="https://github.com/kyverno/kyverno/releases/download/v${VERSION}/${BINARY}"
