123bfb9ea1687c4633990b5a8f3590767b5d86ef
Trojanul <trojanul2008@gmail.com>
2025-08-09 18:36:45 +0300

    fix: correct Kyverno CLI asset URL

--- scripts/install/install-kyverno-cli.sh
+++ scripts/install/install-kyverno-cli.sh
@@ -4,9 +4,9 @@ source ./scripts/utils/load-version.sh
 
 VERSION=$(get_version kyvernoApp)   # e.g., 1.12.4
 OS="linux"
-ARCH="amd64"  # CI uses x64 runners; use arch detection if needed
+ARCH="amd64"
 
-BINARY="kyverno_${VERSION}_${OS}_${ARCH}.tar.gz"
+BINARY="kyverno-cli_v${VERSION}_${OS}_${ARCH}.tar.gz"
 URL="https://github.com/kyverno/kyverno/releases/download/v${VERSION}/${BINARY}"
 
 echo "📥 Downloading Kyverno CLI from: $URL"
